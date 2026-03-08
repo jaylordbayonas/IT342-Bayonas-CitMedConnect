2fba74709b8ce447fc7bef6009215a1d52ebd20d
Jaylord Bayonas <jaylordbayonas87@gmail.com>
2026-03-08 22:57:26 +0800

    Initial Spring Boot Project Setup

---
+++ citmedconnect/src/main/java/edu/cit/bayonas/citmedconnect/CitmedconnectApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.bayonas.citmedconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitmedconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitmedconnectApplication.class, args);
+	}
+
+}
---
+++ citmedconnect/src/test/java/edu/cit/bayonas/citmedconnect/CitmedconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.bayonas.citmedconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitmedconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
